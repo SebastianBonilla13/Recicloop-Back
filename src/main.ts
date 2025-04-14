6dda5504e90829529c5f0a77627787a9a6ebc9e0
Sebastian Bonilla <jsbonilla@unicauca.edu.co>
2025-04-14 00:52:59 -0500

    activacion CORS Http request front

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
+  
+
   /* const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000); */
 
   const app = await NestFactory.create(AppModule); // carga raiz (controladores, servicios, etc.). representando la app
+
+
+  // Configuración de CORS (Cross-Origin Resource Sharing)
+  app.enableCors({
+    origin: 'http://localhost:4200', // o '*' para permitir todos los orígenes
+    credentials: true, // si usas cookies o autenticación
+  });
+
 
   // Prefijo global para organizar rutas
   app.setGlobalPrefix("api/v1");
